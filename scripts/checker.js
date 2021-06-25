fab5c6de8bc2b7160275bd648026b9fa78426641
browjob <32989588+browjob@users.noreply.github.com>
2021-06-25 15:20:43 +0300

    update

--- scripts/checker.js
+++ scripts/checker.js
@@ -27,6 +27,8 @@ const urls =
     `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCf7GcWt0V6_f8A5UzO48iLw`, // Olisya TS
     `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCgGWdn-B7XA2ahisWUyCYHg`, // Палата 23    
     `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCSlgBdZxScyvtEzuZ-x0ekg`, // Из Москвы в Европу. Жизнь с нуля    
+    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCcRoDJkE4FDDZUTzcBnsnPA`, // никит очка    
+    
 ];
 
 function LiveChecker() {

2df0d56083ce75ed72c223b9f60066cb486ccc0c
Billy <billy@gmail.com>
2021-07-11 13:36:38 +0300

    Fixed

--- scripts/checker.js
+++ scripts/checker.js
@@ -1,50 +1,166 @@
-const urls =
-[
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCN6GNZY8cAhCc1ewXGyDb9A`, // Tupa ez
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCNcuCP5IAI6My3e5Z-vKkBQ`, // VjLink Officical
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCOrraQ1wIEdkHCtZq3zH2oA`, // VjLink НАРЕЗКИ
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCpUpSXoni2C5bp9eBzMnUPA`, // Stethem Team
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCWB2jzhj926UKj9b4bXPttw`, // Stethem Stream
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCBdN3wEYOQ9nJR3BIy9NXxQ`, // Олег Бандит
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCuVzDm4YqXEhLE_RvVT5GAg`, // Kupala TV
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCKaO5Kvxpx6xqNWJN5HGJww`, // АндрейиКо03
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCrEufIi5_5Yn0rk3BrbrFDQ`, // Ваня Филатов
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCZfy_7AcFdTsQWHa6Rb1xGw`, // Вова Фунтик
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UC9mdbw7nIvgQbc83o55IRLg`, // ГОБЗ ШОУ
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCv3z49ZPnPEOnhEkfwPfsVA`, // Михаил Горлачёв свобода от зависимости
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCMsk-hrES4Vr_0ff_0XfJtw`, // ШПУНТ И БРИГАДА
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCKAW3U9zWu-FEL6M28nGmiQ`, // Сутулый почтальон
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCTZ3JhCwdjf9_3P0s6XltEA`, // 47 Хромосома
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCl08lntwp6L8sGX3dUWFqtg`, // Герман Ягодка
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCY6tkAMYby2XJoL8LGj1obQ`, // Tupa Trezvui splash
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCLFIzo0u7Z4mmxJRxpdPhWQ`, // Кирюша LIVE
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCTaj82bdEJ8EzYYmygsL7GQ`, // СЕКТА
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCDxns5jCJ09P4zCe_Kpyblg`, // Бертолет
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCqGZd3yTxWqnXxBm6J9FTlA`, // Tupa Splash
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCMG0wa7TcGGzfVJu2CmVxhQ`, // OZON671GAMES3
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UClzAjKnWSVEXwImRiIEVQ0w`, // Matilda Lando
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCXdyP8qQf26lsADsGp-itjw`, // АндрюхаиКомпания
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCf7GcWt0V6_f8A5UzO48iLw`, // Olisya TS
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCgGWdn-B7XA2ahisWUyCYHg`, // Палата 23    
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCSlgBdZxScyvtEzuZ-x0ekg`, // Из Москвы в Европу. Жизнь с нуля    
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCcRoDJkE4FDDZUTzcBnsnPA`, // никит очка    
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCbTLDSLh1_PNgmFMJ4v-xHg`, // Арчи Чиллов    
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCYYvf2FsWtLgF2Py42NAqNQ`, // НОРСКИЕ ДВИЖЕНИЯ
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCILhV84IVElDxKa_9mGVpTA`, // ДОРОГОЮ ДОБРА
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCSeTnvtWSsVuBJ1Ghycj4Lg`, // Бандит ТВ
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCie_eC-Gk1ap47VUyYdOVYA`, // Коляска
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCXugNh7Ec66p_iYBQPfWd9Q`, // Мопсихи NEW
-    `https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCVE57NdL52xkeE9j77pnPng`, // БУ Lebed & Shaman
-];
+const urls = [
+    {
+       "channelname":"Tupa ez",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCN6GNZY8cAhCc1ewXGyDb9A"
+    },
+    {
+       "channelname":"VjLink Officical",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCNcuCP5IAI6My3e5Z-vKkBQ"
+    },
+    {
+       "channelname":"VjLink НАРЕЗКИ",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCOrraQ1wIEdkHCtZq3zH2oA"
+    },
+    {
+       "channelname":"Stethem Team",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCpUpSXoni2C5bp9eBzMnUPA"
+    },
+    {
+       "channelname":"Stethem Stream",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCWB2jzhj926UKj9b4bXPttw"
+    },
+    {
+       "channelname":"Олег Бандит",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCBdN3wEYOQ9nJR3BIy9NXxQ"
+    },
+    {
+       "channelname":"Kupala TV",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCuVzDm4YqXEhLE_RvVT5GAg"
+    },
+    {
+       "channelname":"АндрейиКо03",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCKaO5Kvxpx6xqNWJN5HGJww"
+    },
+    {
+       "channelname":"Ваня Филатов",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCrEufIi5_5Yn0rk3BrbrFDQ"
+    },
+    {
+       "channelname":"Вова Фунтик",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCZfy_7AcFdTsQWHa6Rb1xGw"
+    },
+    {
+       "channelname":"ГОБЗ ШОУ",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UC9mdbw7nIvgQbc83o55IRLg"
+    },
+    {
+       "channelname":"Михаил Горлачёв свобода от зависимости",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCv3z49ZPnPEOnhEkfwPfsVA"
+    },
+    {
+       "channelname":"ШПУНТ И БРИГАДА",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCMsk-hrES4Vr_0ff_0XfJtw"
+    },
+    {
+       "channelname":"Сутулый почтальон",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCKAW3U9zWu-FEL6M28nGmiQ"
+    },
+    {
+       "channelname":"47 Хромосома",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCTZ3JhCwdjf9_3P0s6XltEA"
+    },
+    {
+       "channelname":"Герман Ягодка",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCl08lntwp6L8sGX3dUWFqtg"
+    },
+    {
+       "channelname":"Tupa Trezvui splash",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCY6tkAMYby2XJoL8LGj1obQ"
+    },
+    {
+       "channelname":"Кирюша LIVE",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCLFIzo0u7Z4mmxJRxpdPhWQ"
+    },
+    {
+       "channelname":"СЕКТА",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCTaj82bdEJ8EzYYmygsL7GQ"
+    },
+    {
+       "channelname":"Бертолет",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCDxns5jCJ09P4zCe_Kpyblg"
+    },
+    {
+       "channelname":"Tupa Splash",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCqGZd3yTxWqnXxBm6J9FTlA"
+    },
+    {
+       "channelname":"Matilda Lando",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCMG0wa7TcGGzfVJu2CmVxhQ"
+    },
+    {
+       "channelname":"АндрюхаиКомпания",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCXdyP8qQf26lsADsGp-itjw"
+    },
+    {
+       "channelname":"Olisya TS",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCf7GcWt0V6_f8A5UzO48iLw"
+    },
+    {
+       "channelname":"Из Москвы в Европу. Жизнь с нуля",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCSlgBdZxScyvtEzuZ-x0ekg"
+    },
+    {
+       "channelname":"никит очка",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCcRoDJkE4FDDZUTzcBnsnPA"
+    },
+    {
+       "channelname":"Арчи Чиллов",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCbTLDSLh1_PNgmFMJ4v-xHg"
+    },
+    {
+       "channelname":"НОРСКИЕ ДВИЖЕНИЯ",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCYYvf2FsWtLgF2Py42NAqNQ"
+    },
+    {
+       "channelname":"ДОРОГОЮ ДОБРА",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCILhV84IVElDxKa_9mGVpTA"
+    },
+    {
+       "channelname":"Бандит ТВ",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCSeTnvtWSsVuBJ1Ghycj4Lg"
+    },
+    {
+       "channelname":"Коляска",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCie_eC-Gk1ap47VUyYdOVYA"
+    },
+    {
+       "channelname":"Мопсихи NEW",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCXugNh7Ec66p_iYBQPfWd9Q"
+    },
+    {
+       "channelname":"Эрогенная зона",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UC0GAzZZgI3aT2PDgggaXWRg"
+    },
+    {
+       "channelname":"Бермудский Наумов",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCdR9kI4nSK8sGL7pcngolyg"
+    },
+    {
+       "channelname":"Елизавета Ветрова",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCO7Hl7pS8z7YbA67IQAsOew"
+    },
+    {
+       "channelname":"AnYaShaTV",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UC3dQuuY3Se-etcg1QjE_wvA"
+    },
+    {
+       "channelname":"Сюр Стрим",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UCQRuHv64UKC6_pGzVqzHKtw"
+    },
+    {
+       "channelname":"Сюр Стрим Трансляции",
+       "url":"https://youtube-api.browjob.repl.co/youtube/v3/getstreams/UC257VuxppVHv6nblsge_Yhw"
+    }
+ ];
 
 function LiveChecker() {
 for (let i = 0; i < urls.length; i++) {
-    fetch(urls[i])
+    fetch(urls[i].url)
         .then((res) => res.json())
         .then((e) => {
 
             if (e.liveNow === true)
-                document.getElementById("LC_ChannelName").innerHTML += `<div id="d" title="${e.title}" onclick="window.open('https://ylbetter.github.io/watch?v=` + e.videoId + `')">` +
+                document.getElementById("LC_ChannelName").innerHTML += `<div id="d" title="${e.title}" onclick="window.open('`+ location.origin +`/watch?v=` + e.videoId + `')">` +
                     `<div class="info"> 
                  <div class="text">   
                 <span class="channelTitle">` + e.author +
